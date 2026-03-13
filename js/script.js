2f3979f8d6971d1864d9f55dc711c11c63b68ffa
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2026-03-13 20:21:18 -0300

    d

--- js/script.js
+++ js/script.js
@@ -5,15 +5,35 @@ const multiplicationTitle = document.querySelector("#multiplication-title span")
 const multiplicationTable = document.querySelector("#multiplication-operations");
 
 //funções
-const createTable = (number,multiplicationNumber)=>{
+const createTable = (number,multiplicatorNumber)=>{
     multiplicationTable.innerHTML = "";
 
-    for(i=1; i<=multiplicationNumber;i++){
+    for(i=1; i<=multiplicatorNumber;i++){
         const result = number*i;
         const template = `<div class="row">
         <div class="operation">${number} x ${i} = </div>
         <div class="result">${result}</div>
         </div>`;
+
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template,text/html);
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
     }
-}
+
+    multiplicationTitle.innerText = number;
+};
+
+//Eventos
+multiplicationForm.addEventListener("submit",(e) => {
+
+    const multiplicationNumber = numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber,multiplicatorNumber);
+
+});
 
